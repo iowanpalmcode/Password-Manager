e34569851d90eec0b4ff9a970fcc29c7f0e372b7
Joseph Rotramel <josephrotramel9@gmail.com>
2026-02-05 22:45:59 -0600

    basic logic/enhancements

---
+++ Front end/script.js
@@ -0,0 +1,9 @@
+document.querySelector(".addPassword").addEventListener('click', () => {
+    
+})
+document.querySelector(".help").addEventListener('mouseover', () => {
+    document.querySelector(".helpDesc").style.display = "block"
+})
+document.querySelector(".help").addEventListener('mouseout', () => {
+    document.querySelector(".helpDesc").style.display = "none"
+})
