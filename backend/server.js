e292f2406bbc4383c6230429561aefca7d44c237
Joseph Rotramel <josephrotramel9@gmail.com>
2026-02-09 22:21:00 -0600

    database setup

--- backend/server.js
+++ backend/server.js
@@ -1,10 +1,22 @@
 import express from "express"
 import path from "path"
 import 'dotenv/config'
+const app = express()
+import mongoose from "mongoose"
 const root = path.resolve(import.meta.dirname, "..")
 
 
-const app = express()
+const connectDB = async function() {
+   try {
+    await mongoose.connect(process.env.MONGO_URI)
+    console.log("DB connected successfully")
+} catch (error) {
+    console.log(error)
+} 
+}
+
+connectDB()
+
 app.use(express.static(path.join(root, "frontend")))
 const PORT = process.env.PORT || 5500
 
@@ -14,4 +26,8 @@ app.listen(PORT, () => {
 
 app.get("/", (req, res) => {
     res.sendFile(path.join(root, "frontend", "index.html"))
+})
+
+app.get("/yope", (req, res) => {
+    res.send()
 })
